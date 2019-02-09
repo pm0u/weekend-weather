ff4f7878aa4c00216b759d7982091ccd487f6ad5
Paul Mourer <paul.mourer@gmail.com>
2019-02-09 12:31:06 -0700

    beautified

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import City from './City'
 class App extends Component {
 
   state = {
-    cities:[['Estes Park','Rocky Mountain National Park'], ['Center','Great Sand Dunes National Park'], ['Ouray','Uncompahgre National Forest']],
+    cities: [['Estes Park', 'Rocky Mountain National Park'], ['Center', 'Great Sand Dunes National Park'], ['Ouray', 'Uncompahgre National Forest']],
   }
 
 
   render() {
-   return (
+    return (
       <>
-      <div className='row'>
-      {this.state.cities.map((city,i)=> {
-      return (<City location={city} key={i}/>)
-      })}
-      </div>
-      <h5 className='center-align' style={{paddingBottom:30}}>What'll it be this weekend?? Type 2 or type 1 fun?</h5>
+        <div className='row'>
+          {this.state.cities.map((city, i) => {
+            return (<City location={city} key={i} />)
+          })}
+        </div>
+        <h5 className='center-align' style={{ paddingBottom: 30 }}>What'll it be this weekend?? Type 2 or type 1 fun?</h5>
       </>
     );
   }
--- src/City.js
+++ src/City.js
@@ -4,7 +4,7 @@ import moment from 'moment'
 export default class City extends Component {
 
     state = {
-        friday:[],
+        friday: [],
         saturday: [],
         sunday: []
     }
@@ -14,62 +14,62 @@ export default class City extends Component {
         await this.setState({ ...forecast })
     }
 
-  getDay = (forecast,daysAhead) => {
-    const dayOfMonth = parseInt(new Date().getDate())
-    const day = forecast.list.filter(hourly => {
-      return moment(hourly.dt*1000).isSame(moment(new Date()).add(daysAhead,'days'), 'day')
-    })
-    return day
-  }
+    getDay = (forecast, daysAhead) => {
+        const dayOfMonth = parseInt(new Date().getDate())
+        const day = forecast.list.filter(hourly => {
+            return moment(hourly.dt * 1000).isSame(moment(new Date()).add(daysAhead, 'days'), 'day')
+        })
+        return day
+    }
 
-  getForecastForCity = async (city) => {
-      const owmAPI = process.env.REACT_APP_OWM_API_KEY
-      const forecast = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city.replace(/' '/%20)},us&appid=${owmAPI}`).then(response => response.json()).catch(data => console.log(data))
-      const friday = this.getDay(forecast, 1)
-      const saturday = this.getDay(forecast, 2)
-      const sunday = this.getDay(forecast, 3)
-      return {friday,saturday,sunday}
-  }
+    getForecastForCity = async (city) => {
+        const owmAPI = process.env.REACT_APP_OWM_API_KEY
+        const forecast = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city.replace(/' '/ % 20)},us&appid=${owmAPI}`).then(response => response.json()).catch(data => console.log(data))
+        const friday = this.getDay(forecast, 1)
+        const saturday = this.getDay(forecast, 2)
+        const sunday = this.getDay(forecast, 3)
+        return { friday, saturday, sunday }
+    }
 
-  tempToF = (kTemp) => {
-      const cTemp = kTemp - 273.15
-      const fTemp = cTemp * (9/5) + 32
-      return fTemp.toFixed(0)
-  }
+    tempToF = (kTemp) => {
+        const cTemp = kTemp - 273.15
+        const fTemp = cTemp * (9 / 5) + 32
+        return fTemp.toFixed(0)
+    }
 
-  dayForecast = (day) => {
-        return this.state[day].map((hour,i) => {
-                return (<li className='collection-item' key={i} style={{display:'flex', justifyContent:'space-between'}}>
-                <span>{`${moment(hour.dt*1000).format('LT')}:`}</span><span> {`${this.tempToF(hour.main.temp)}°F`}</span><span>{`${hour.weather[0].description}`}</span>
+    dayForecast = (day) => {
+        return this.state[day].map((hour, i) => {
+            return (<li className='collection-item' key={i} style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <span>{`${moment(hour.dt * 1000).format('LT')}:`}</span><span> {`${this.tempToF(hour.main.temp)}°F`}</span><span>{`${hour.weather[0].description}`}</span>
                 <img className='right-align' src={`http://openweathermap.org/img/w/${hour.weather[0].icon}.png`} />
-                </li>)
-            })
-  }
+            </li>)
+        })
+    }
 
-  render() {
-    return (
-        <>
-        <div className='col s12 l4 m4'>
-        <div className="card blue-grey lighten-5">
-        <div className="card-content">
-          <span className="card-title"><h4>Forecast for {this.props.location[0]}</h4></span>
-          <h5>Near {this.props.location[1]}</h5>
-          <ul className='collection with-header'>
-            <li className='collection-header'>Friday</li>
-            {this.dayForecast('friday')}
-          </ul>
-          <ul className='collection with-header'>
-            <li className='collection-header'>Saturday</li>
-            {this.dayForecast('saturday')}
-          </ul>
-          <ul className='collection with-header'>
-            <li className='collection-header'>Sunday</li>
-            {this.dayForecast('sunday')}
-          </ul>
-        </div>
-        </div>
-        </div>
-        </>
-    )
-  }
+    render() {
+        return (
+            <>
+                <div className='col s12 l4 m4'>
+                    <div className="card blue-grey lighten-5">
+                        <div className="card-content">
+                            <span className="card-title"><h4>Forecast for {this.props.location[0]}</h4></span>
+                            <h5>Near {this.props.location[1]}</h5>
+                            <ul className='collection with-header'>
+                                <li className='collection-header'>Friday</li>
+                                {this.dayForecast('friday')}
+                            </ul>
+                            <ul className='collection with-header'>
+                                <li className='collection-header'>Saturday</li>
+                                {this.dayForecast('saturday')}
+                            </ul>
+                            <ul className='collection with-header'>
+                                <li className='collection-header'>Sunday</li>
+                                {this.dayForecast('sunday')}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
